2eaa03ca79127d7c97d521f1bb40cc7cd2e51313
subhmish <subhransu.mishra@gmail.com>
2022-10-05 11:15:22 +0530

    fourth commit

--- code/temperature_index_plot.R
+++ code/temperature_index_plot.R
@@ -6,6 +6,8 @@ read_csv("data/GLB.Ts+dSST.csv", skip=1,na="***") %>%
   geom_line(color = "gray", size =0.5) +
   geom_point(fill = "white", color = "red", shape = 21) +
   geom_smooth(se = FALSE, color = "black",size = 0.5, span = 0.50) +
+  scale_x_continuous(breaks=seq(1880,2023,20))+
+  scale_y_continuous(limits = c(-0.5,1.5))+
   theme_light()
 
 ggsave("figures/tempearature_index_plot.png",width = 6, height = 4)
