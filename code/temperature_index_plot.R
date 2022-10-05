bdfb1d5f7224d1686d4a7bc33c7ad79747233af7
subhmish <subhransu.mishra@gmail.com>
2022-10-05 16:49:28 +0530

    eighth

--- code/temperature_index_plot.R
+++ code/temperature_index_plot.R
@@ -3,24 +3,33 @@ library(tidyverse)
 read_csv("data/GLB.Ts+dSST.csv", skip=1,na="***") %>%
   select(year = Year, t_diff = `J-D`) %>%
   ggplot(aes(x = year, y = t_diff)) + 
-  geom_line(color = "gray", size =0.5) +
-  geom_point(fill = "white", color = "red", shape = 21) +
-  geom_smooth(se = FALSE, color = "black",size = 0.5, span = 0.50) +
-  scale_x_continuous(breaks=seq(1880,2023,20), expand = c(0,0))+
-  scale_y_continuous(limits = c(-0.5,1.5), expand = c(0,0))+
-  labs(
-    x="YEAR",
-    y="Temperature anomaly(C)",
+  geom_line(aes(color = "1"), size =0.5, show.legend = FALSE) +
+  geom_point(fill = "white", aes(color = "1"), shape = 21, show.legend = TRUE) +
+  geom_smooth(se = FALSE, aes(color = "2") ,size = 0.5, span = 0.50) +
+  scale_x_continuous(breaks=seq(1880,2023,20), expand = c(0,0)) +
+  scale_y_continuous(limits = c(-0.5,1.5), expand = c(0,0)) +
+  scale_color_manual(name=NULL,
+                     breaks = c(1,2),
+                     values = c("gray","black"),
+                     labels= c("Annual mean","Lowess smoothing"),
+                     guide = guide_legend(override.aes = list(shape =15))
+                     ) +
+  labs( x="YEAR", y="Temperature anomaly(C)",
     title = "GLOBAL LAND-OCEAN TEMPEATURE INDEX",
-    subtitles = "Data Source: NASA's GISS"
+    subtitles = "Data Source: NASA's GISS. Credit NASA-GISS"
   ) +
   theme_light() +
   theme(axis.ticks = element_blank(),
         plot.title.position = "plot",
-        plot.title=element_text(margin = margin(b=15), color="red", face = "bold"),
-        plot.subtitle = element_text(size =8)
+        plot.title = element_text(margin = margin(b=15), color="red", face = "bold"),
+        plot.subtitle = element_text(size =8, margin = margin(b=10)),
+        legend.position = c(0.1,0.9),
+        legend.title = element_text(size=.2),
+        legend.key.height = unit(10, "pt"),
+        legend.margin = margin(0,0,0,0)
         )
 
 ggsave("figures/tempearature_index_plot.png",width = 6, height = 4)
+
 
 
