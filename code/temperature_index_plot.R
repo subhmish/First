51cc9320b76115ca23b1492eb0f9e470b4e43cdd
subhmish <subhransu.mishra@gmail.com>
2022-10-05 14:24:21 +0530

    fifth

--- code/temperature_index_plot.R
+++ code/temperature_index_plot.R
@@ -6,8 +6,17 @@ read_csv("data/GLB.Ts+dSST.csv", skip=1,na="***") %>%
   geom_line(color = "gray", size =0.5) +
   geom_point(fill = "white", color = "red", shape = 21) +
   geom_smooth(se = FALSE, color = "black",size = 0.5, span = 0.50) +
-  scale_x_continuous(breaks=seq(1880,2023,20))+
-  scale_y_continuous(limits = c(-0.5,1.5))+
-  theme_light()
+  scale_x_continuous(breaks=seq(1880,2023,20), expand = c(0,0))+
+  scale_y_continuous(limits = c(-0.5,1.5), expand = c(0,0))+
+  labs(
+    x="YEAR",
+    y="Temperature anomaly(C)",
+    title = "GLOBAL LAND-OCEAN TEMPEATURE INDEX",
+    subtitles = "Data Source: NASA's GISS"
+  )
+  theme_light() +
+  theme(axis.ticks = element_blank())
 
 ggsave("figures/tempearature_index_plot.png",width = 6, height = 4)
+
+
